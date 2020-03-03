045f0cbe1c181ad35445ebed8e2e1381f57dd4d1
Debdut Goswami <debdutgoswami@gmail.com>
2020-03-04 00:19:00 +0530

    ADD app

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, request, make_response, send_file
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        # caching the content for 10 min
+        template = render_template('home.html')
+        response = make_response(template)
+        response.headers['Cache-Control'] = 'public, max-age=300, s-maxage=600'
+
+        return response
+    else:
+        dept, email, year = dict(request.form)['dept'], dict(request.form)['email'], dict(request.form)['year']
+        try:
+            return send_file(os.path.join(os.path.dirname(__file__),"certificate",f"{year} {dept}",f"{email}.pdf"))
+        except FileNotFoundError:
+            return "file not found"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
